8011d142ff4cc045fe4325ee98a01908a0e27c96
Schamberger12 <32084866+Schamberger12@users.noreply.github.com>
2019-09-14 17:46:08 -0500

    Button, Spokesperson, bunch of assets

---
+++ Assets/Scripts/AudioTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+
+    public AudioSource audioData;
+
+    private void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+}
---
+++ Assets/Scripts/SpokesPerson.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia; 
+
+public class SpokesPerson : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public AudioSource audioData;
+
+    public GameObject piggy;
+
+    public GameObject vButtonSpeak;
+
+    bool isPlaying; 
+
+    private void Awake()
+    {
+        audioData = piggy.GetComponent<AudioSource>();
+        piggy = GameObject.Find("SpokesPerson");
+        vButtonSpeak = GameObject.Find("ARButton");
+        vButtonSpeak.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+        isPlaying = false;
+
+
+    }
+
+    private void Start()
+    {
+        piggy.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isPlaying && !audioData.isPlaying)
+        {
+            isPlaying = false;
+            audioData.Stop();
+            piggy.SetActive(false); 
+        }
+    }
+
+    /// <summary>
+    /// Called when the virtual button has just been pressed:
+    /// </summary>
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+
+
+        if (!isPlaying)
+        {
+            isPlaying = true;
+            piggy.SetActive(true); 
+            Debug.Log("button Pressed");
+            audioData.Play();
+            if (audioData.isPlaying)
+                Debug.Log("Sound Playing"); 
+
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+}
+
